ab513885fb74f5ff65f0b3d46676a14b8b63c9b6
martiert <martiert@ifi.uio.no>
2009-10-13 14:07:38 +0000

    Worked a bit with the vector

--- PandoraCore/include/PandoraVector.h
+++ PandoraCore/include/PandoraVector.h
@@ -9,7 +9,7 @@ namespace Pandora
     class Vector
     {
         public:
-            Vector(const size_t size = 4, const size_t inc = 4);
+            Vector(const size_t size = 1, const size_t inc = 1);
             ~Vector();
 
             //Get the number of elements in the vector
@@ -92,17 +92,55 @@ namespace Pandora
     T& Vector<T>::operator[](const size_t i)
     {
         assert(i < m_size && "Index out of bounds");
+
         return m_data[i];
     }
 
     //-------------------------------------------------------------------------
-    // Access constant element number i
+    // Access constant element number i.
     //-------------------------------------------------------------------------
     template<class T>
     const T& Vector<T>::operator[](const size_t i) const
     {
         assert(i < m_size && "Index out of bounds");
         return m_data[i];
+    }
+
+    //-------------------------------------------------------------------------
+    // Set this element to the first free element in the vector.
+    //
+    // If we have free places, we add to the first free place we find. If we 
+    // have a full array, we make a new array which is size + increment large. 
+    // then we move all the data to the new vector, and add the new element to 
+    // the end of the vector.
+    //-------------------------------------------------------------------------
+    template<class T>
+    void Vector<T>::append(const T& element)
+    {
+        if(m_elements < m_size) {
+            int i = 0;
+
+            while(m_data[i])
+                ++i;
+
+            m_data[i] = element;
+            ++m_elements;
+        } else {
+            assert(m_increment != 0 && "Can't increment the vector");
+
+            T *tmp = new T[m_size + m_increment];
+
+            //Move data to the temporary array
+            for(int i = 0; i < m_size; ++i) {
+                tmp[i] = m_data[i];
+            }
+
+            delete[] m_data;
+
+            m_data = tmp;
+            m_data[m_elements++] = element;
+            m_size += m_increment;
+        }
     }
 }
 
