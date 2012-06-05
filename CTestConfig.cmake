0e01d95bce5fc0f93c431fca82d30764bb155c22
Martin Ertsaas <martiert@gmail.com>
2012-06-05 09:55:28 +0200

    Added a ctest config for submitting to cdash

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,16 +1,15 @@
 ## This file should be placed in the root directory of your project.
 ## Then modify the CMakeLists.txt file in the root directory of your
 ## project to incorporate the testing dashboard.
-## # The following are required to uses Dart and the Cdash dashboard
-##   ENABLE_TESTING ()
-##   INCLUDE (CTest)
-set (CTEST_PROJECT_NAME "Pandora3D")
-set (CTEST_NIGHTLY_START_TIME "00:00:00 EST")
+##
+## # The following are required to submit to the CDash dashboard:
+##   ENABLE_TESTING()
+##   INCLUDE(CTest)
 
-set (CTEST_DROP_METHOD "http")
-set (CTEST_DROP_SITE "my.cdash.org")
-set (CTEST_DROP_LOCATION "/submit.php?project=Pandora3D")
-set (CTEST_DROP_SITE_CDASH TRUE)
+set(CTEST_PROJECT_NAME "Pandora")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
 
-set (UPDATE_COMMAND "${GITCOMMAND}")
-set (UPDATE_OPTIONS "${GIT_UPDATE_OPTIONS})
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=Pandora")
+set(CTEST_DROP_SITE_CDASH TRUE)
