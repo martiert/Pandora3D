7f707d8f196fd82559e93b4044bdf5dfd95cc3d7
Martin Ertsaas <martiert@gmail.com>
2012-11-04 13:05:31 +0100

    Dont run tests if build fails.

--- runalltests.sh
+++ runalltests.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
+
+function check_return_value()
+{
+  if [ $1 -ne 0 ];
+  then
+    exit 1
+  fi
+}
+
 mkdir _build_/test-reports -p
 cd _build_
 cmake ..
+check_return_value $?
+
 make -j8
+check_return_value $?
+
 ./bin/mathtests --gtest_output=xml:test-reports/mathreport.xml
 ./bin/physicstests --gtest_output=xml:test-reports/physicsreport.xml
