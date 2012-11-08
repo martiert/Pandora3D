2fd94c7a4fe1fb2e8eb6e1b3247f7d6522e97b1a
Martin Ertsaas <martiert@gmail.com>
2012-11-08 21:41:59 +0100

    runalltests.sh: Prefer clang as a compiler, forward all arguments to make.
    We prefer clang as the error output given by clang is brilliant, and it
    contains a static code analyser (Just run 'scan-build make').
    All arguments, like -j will now be forwarded to make.
    
    Signed-off-by: Martin Ertsaas <martiert@gmail.com>

--- runalltests.sh
+++ runalltests.sh
@@ -8,12 +8,20 @@ function check_return_value()
   fi
 }
 
+tmp=$(which clang)
+
+if [ $? -eq 0 ];
+then
+  export CC=clang
+  export CXX=clang++
+fi
+
 mkdir _build_/test-reports -p
 cd _build_
 cmake ..
 check_return_value $?
 
-make -j8
+make $@
 check_return_value $?
 
 ./bin/mathtests --gtest_output=xml:test-reports/mathreport.xml
